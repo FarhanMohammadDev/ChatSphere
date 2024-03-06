42a6650bf86eb75ded791697a959063d4217507f
FarhanMohammadDev <farhanmohammad0677@gmail.com>
2024-03-06 13:13:31 +0100

    server ranning on localhost:5000

--- server/index.ts
+++ server/index.ts
@@ -1,1 +1,23 @@
-console.log(" hello server ");
+import express, { Request, Response } from 'express';
+import cors from 'cors';
+import "dotenv/config";
+// import connectDB from './configs/db';
+
+// initialisation App
+const app = express();
+
+// apply Middlewares
+app.use(express.json())
+
+app.use(express.urlencoded({extended: true})) // parse data formulaire 
+app.use(cors())
+
+app.get("",async (req: Request, res: Response)=> {
+        res.json({ message: "Hello from express endpoint"})
+    }
+ )
+
+const PORT = process.env.PORT || 8080;
+app.listen(PORT,()=>{console.log("server ranning on localhost:5000")})
+
+// connectDB()
